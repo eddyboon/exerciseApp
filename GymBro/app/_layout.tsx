c5bb443292a0876a268ab1d2cfeca92031de615d
eddyboon <66462284+eddyboon@users.noreply.github.com>
2024-10-07 17:03:39 +1100

    skeleton code

---
+++ GymBro/app/_layout.tsx
@@ -0,0 +1,15 @@
+import { useColorScheme } from "@/hooks/useColorScheme";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
